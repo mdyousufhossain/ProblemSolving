39cee5dacfa48e0b48d3d68440579d1fa24e2c99
mdyousufhossain <yousafhossain3@gmail.com>
2022-07-05 23:22:04 +0600

    dude

---
+++ cp/a.js
@@ -0,0 +1,17 @@
+const mycup = {
+    size : 4.65,
+    type : 'tea',
+    usedfor: 'drink',
+    item : 'blood',
+    usedby: 'vimpire'
+}
+
+
+const student = [{ name : 'bal',roll:123213 ,}]
+
+
+const arr = [1,2,3,4]
+
+const nar = arr.map((x) => x+x )
+
+console.log(nar)
