c700a9be4bf6ca4adeb657a459812d38b1576dfa
mdyousufhossain <yousafhossain3@gmail.com>
2022-07-28 13:13:29 +0600

    mapjs

--- cp/a.js
+++ cp/a.js
@@ -15,3 +15,46 @@ const arr = [1,2,3,4]
 const nar = arr.map((x) => x+x )
 
 console.log(nar)
+
+
+function callme(callback){
+    console.log('beeeh')
+    if (callback instanceof Function){
+        callback()
+    }
+}
+
+
+function bee(){
+    console.log('die die')
+}
+
+callme(bee)
+
+function multiplayer(factor){
+    return function(x){
+        return x * factor
+    }
+}
+
+double = multiplayer(2)
+
+console.log(double(2))
+
+
+let arr2 = [-1,3,5,6,8,0,1,9,1004,0x1df]
+
+console.log(arr2)
+
+let fsort =  arr2.sort((a,b) => b-a)
+console.log(fsort)
+let fsort1 =  arr2.sort((a,b) => a-b)
+
+var val = 'bal'
+var bal = arr2.forEach(x => x )
+console.log(val.match)
+
+console.log(bal)
+
+ 
+
