00293e89d713c94c543d2773df3365c422f28a8f
mdyousufhossain <yousafhossain3@gmail.com>
2022-12-18 02:37:04 +0600

    cats project added

--- Javascript/Prototype/scalable.js
+++ Javascript/Prototype/scalable.js
@@ -1,19 +1,37 @@
+// function creatingElemet(type){
+//   const elm = document.createElement(type)
+//   elm.innerText = "its loading"
+// }
+
+
+
 function createingText(newText){
     const CatFact = document.getElementById('catFact')
-  
+
    return CatFact.innerText = newText
 
 }
-const Matrix = []
+
+function MakingElVisible(StyleAttr,el){
+  let gettingCats = document.getElementById(el);
+
+  return gettingCats.style.display = StyleAttr ;
+
+}
+
 
 
 async function getData() {
+   MakingElVisible("block","invisible")
+   MakingElVisible("none","SleepingCats")
     try {
       const response = await fetch('https://catfact.ninja/fact');
       const datas = await response.json();
-      const data = datas.fact 
+      const data = datas.fact
+      MakingElVisible("none","invisible")
+      MakingElVisible("block","SleepingCats")
       createingText(data)
-        
+
     } 
     
     catch (error) {
